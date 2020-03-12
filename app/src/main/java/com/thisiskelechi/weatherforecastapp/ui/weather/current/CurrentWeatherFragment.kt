86fe28d3eb43f9099a5825ee8694a4b533a5c3af
riskymind <oparaiskelechi@gmail.com>
2020-03-12 12:21:56 +0100

    made the network call and ready for consumption

--- app/src/main/java/com/thisiskelechi/weatherforecastapp/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/thisiskelechi/weatherforecastapp/ui/weather/current/CurrentWeatherFragment.kt
@@ -2,12 +2,20 @@ package com.thisiskelechi.weatherforecastapp.ui.weather.current
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 import com.thisiskelechi.weatherforecastapp.R
+import com.thisiskelechi.weatherforecastapp.data.ApixuWeatherApiService
+import kotlinx.android.synthetic.main.current_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -28,7 +36,17 @@ class CurrentWeatherFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
+
         // TODO: Use the ViewModel
+        val apiService = ApixuWeatherApiService()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val currentWeatherResponse = apiService.getCurrentWeather("London").await()
+            Log.d("someThing", currentWeatherResponse.toString())
+//            textView.text = currentWeatherResponse.currentWeatherEntry.toString()
+            textView.text = currentWeatherResponse.toString()
+        }
     }
 
 }
